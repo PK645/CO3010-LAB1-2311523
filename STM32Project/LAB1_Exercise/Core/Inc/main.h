0ffe608e87e336ccc232eedcff405258d877588d
Nguyen Phuc Khanh <khanh.nguyenbusiness@hcmut.edu.vn>
2025-09-23 22:54:48 +0700

    no message

--- STM32Project/LAB1_Exercise/Core/Inc/main.h
+++ STM32Project/LAB1_Exercise/Core/Inc/main.h
@@ -57,32 +57,30 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define LED_RED1_Pin GPIO_PIN_2
-#define LED_RED1_GPIO_Port GPIOA
-#define LED_YELLOW1_Pin GPIO_PIN_3
-#define LED_YELLOW1_GPIO_Port GPIOA
-#define LED_GREEN1_Pin GPIO_PIN_4
-#define LED_GREEN1_GPIO_Port GPIOA
-#define LED_RED2_Pin GPIO_PIN_5
-#define LED_RED2_GPIO_Port GPIOA
-#define LED_YELLOW2_Pin GPIO_PIN_6
-#define LED_YELLOW2_GPIO_Port GPIOA
-#define LED_GREEN2_Pin GPIO_PIN_7
-#define LED_GREEN2_GPIO_Port GPIOA
-#define LED7_A_Pin GPIO_PIN_0
-#define LED7_A_GPIO_Port GPIOB
-#define LED7_B_Pin GPIO_PIN_1
-#define LED7_B_GPIO_Port GPIOB
-#define LED7_C_Pin GPIO_PIN_2
-#define LED7_C_GPIO_Port GPIOB
-#define LED7_D_Pin GPIO_PIN_3
-#define LED7_D_GPIO_Port GPIOB
-#define LED7_E_Pin GPIO_PIN_4
-#define LED7_E_GPIO_Port GPIOB
-#define LED7_F_Pin GPIO_PIN_5
-#define LED7_F_GPIO_Port GPIOB
-#define LED7_G_Pin GPIO_PIN_6
-#define LED7_G_GPIO_Port GPIOB
+#define LED1_Pin GPIO_PIN_4
+#define LED1_GPIO_Port GPIOA
+#define LED2_Pin GPIO_PIN_5
+#define LED2_GPIO_Port GPIOA
+#define LED3_Pin GPIO_PIN_6
+#define LED3_GPIO_Port GPIOA
+#define LED4_Pin GPIO_PIN_7
+#define LED4_GPIO_Port GPIOA
+#define LED5_Pin GPIO_PIN_8
+#define LED5_GPIO_Port GPIOA
+#define LED6_Pin GPIO_PIN_9
+#define LED6_GPIO_Port GPIOA
+#define LED7_Pin GPIO_PIN_10
+#define LED7_GPIO_Port GPIOA
+#define LED8_Pin GPIO_PIN_11
+#define LED8_GPIO_Port GPIOA
+#define LED9_Pin GPIO_PIN_12
+#define LED9_GPIO_Port GPIOA
+#define LED10_Pin GPIO_PIN_13
+#define LED10_GPIO_Port GPIOA
+#define LED11_Pin GPIO_PIN_14
+#define LED11_GPIO_Port GPIOA
+#define LED12_Pin GPIO_PIN_15
+#define LED12_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
 
